e8fe6225575bba784579c0b550c0e9c776baf141
atulya-kairati <atulyakairati@gmail.com>
2023-05-09 17:22:49 +0530

    state hoisting

--- app/src/main/java/com/atulya/codapizza/model/Pizza.kt
+++ app/src/main/java/com/atulya/codapizza/model/Pizza.kt
@@ -3,6 +3,17 @@ package com.atulya.codapizza.model
 data class Pizza(
     val toppings: Map<Topping, ToppingPlacement> = emptyMap()
 ){
+
+    val price: Double
+        get() = 9.99 + toppings.asSequence().sumOf { topping ->
+            val (_, toppingPlacement) = topping
+
+            when (toppingPlacement){
+                ToppingPlacement.Left, ToppingPlacement.Right -> 0.5
+                ToppingPlacement.All -> 1.0
+            }
+        }
+
     fun withTopping(topping: Topping, placement: ToppingPlacement?): Pizza{
         return this.copy(
             toppings = if (placement == null){
